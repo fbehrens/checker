aeeabb5064da8333f1cd9cd9d9ef939b812d4eac
Frank Behrens <fbehrens@gmail.com>
2018-04-01 13:53:15 +0200

    use FSCheck

--- src/checkerTests/fileTests.fs
+++ src/checkerTests/fileTests.fs
@@ -3,9 +3,14 @@ open Swensen.Unquote
 open Expecto
 open Checker.File
 
+let config = { FsCheckConfig.defaultConfig with maxTest = 10000 }
+
 [<Tests>]
 let tests =
   testList "file" [
       test "foo" { foo =! "bar" }
-      test "foo1" { foo =! "bar" }
+      testProperty "commutative" <| fun a b ->
+        a + b = b + a
+      testPropertyWithConfig config  "commutative10000" <| fun a b ->
+        a + b = b + a
   ]
