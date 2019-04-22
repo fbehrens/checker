71c95899527c1ea27545aeb7af26cf9522b7ccad
Frank Behrens <frank.behrens@bayer.com>
2019-04-22 20:08:14 +0200

    upgrade fake5

--- build.fsx
+++ build.fsx
@@ -1,32 +1,57 @@
-(* -- Fake Dependencies paket-inline
-source https://api.nuget.org/v3/index.json
+#r "paket: groupref build //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
 
-nuget Fake.Core.Target prerelease
-nuget FSharp.Core prerelease
--- Fake Dependencies -- *)
 
-#r @"packages/build/FAKE/tools/FakeLib.dll"
+let dnOptions =
+  let localPath = Path.getFullName "."
+  DotNet.Options.withWorkingDirectory localPath >> ( Some DotNet.Verbosity.Minimal |> DotNet.Options.withVerbosity )
+
+let runDotNet cmd args =
+    let result = DotNet.exec dnOptions cmd args
+    if result.ExitCode <> 0 then failwithf "'dotnet %s %s failed(%i)" cmd args result.ExitCode
+
+
+Target.create "Clean" (fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs
+)
+
+Target.create "Build" (fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (runDotNet "build")
+)
+Target.runOrDefault "Build"
+
+
+(*
+#r "paket: groupref build //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+// #r @"C:\Users\imynv\.nuget\packages\fake\4.64.14\tools\FakeLib.dll"
+
+open Fake.Core
 open System
 open System.Diagnostics
 
-open Fake
 
 let mutable dotnetCliPath = "dotnet"
 let dotnetCliVersion = "2.1.103"
 
-let runDotNet args =
-  let proc (info : ProcessStartInfo) =
-    info.FileName <- dotnetCliPath
-    info.WorkingDirectory <- "."
-    info.Arguments <- args
-  let result = ProcessHelper.ExecProcess proc TimeSpan.MaxValue
-  if result <> 0 then failwithf "dotnet %s failed" args
+let runDotNet args = ()
+//   let proc (info : ProcessStartInfo) =
+//     info.FileName <- dotnetCliPath
+//     info.WorkingDirectory <- "."
+//     info.Arguments <- args
+//   let result = ProcessHelper.ExecProcess proc TimeSpan.MaxValue
+//   if result <> 0 then failwithf "dotnet %s failed" args
 
-Target "Clean" <| fun _ ->
-  !!"**/bin"++"**/obj" |> CleanDirs
-
-Target  "InstallDotNet" <| fun _ ->
-  dotnetCliPath <- DotNetCli.InstallDotNetSDK dotnetCliVersion
 
 Target "Build" <| fun _ ->
   runDotNet "build"
@@ -41,3 +66,5 @@ Target "Test" <| fun _ ->
   ==> "Test"
 
 RunTargetOrDefault "Test"
+
+*)
